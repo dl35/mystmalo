8042683b1f44e81f8aea4e4e9a4885905d210f59
 lesech35230 <denis.lesech@gmail.com>
2022-06-01 20:32:42 +0200

    update

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app';
+  private path: string = "../../assets/pictos";
+  constructor(private domSanitizer: DomSanitizer, private matIconRegistry: MatIconRegistry) {  
+    this.matIconRegistry.addSvgIcon("E", this.setPath(`${this.path}/E.svg`) ); 
+    this.matIconRegistry.addSvgIcon("ENE", this.setPath(`${this.path}/ENE.svg`)); 
+    this.matIconRegistry.addSvgIcon("ESE", this.setPath(`${this.path}/ESE.svg`)); 
+    this.matIconRegistry.addSvgIcon("N", this.setPath(`${this.path}/N.svg`)); 
+    this.matIconRegistry.addSvgIcon("NE", this.setPath(`${this.path}/NE.svg`)); 
+    this.matIconRegistry.addSvgIcon("NNE", this.setPath(`${this.path}/NNE.svg`)); 
+    this.matIconRegistry.addSvgIcon("NNO", this.setPath(`${this.path}/NNO.svg`)); 
+    this.matIconRegistry.addSvgIcon("NO", this.setPath(`${this.path}/NO.svg`)); 
+    this.matIconRegistry.addSvgIcon("O", this.setPath(`${this.path}/O.svg`)); 
+    this.matIconRegistry.addSvgIcon("ONO", this.setPath(`${this.path}/ONO.svg`)); 
+    this.matIconRegistry.addSvgIcon("OSO", this.setPath(`${this.path}/OSO.svg`)); 
+    this.matIconRegistry.addSvgIcon("S", this.setPath(`${this.path}/S.svg`)); 
+    this.matIconRegistry.addSvgIcon("SE", this.setPath(`${this.path}/SE.svg`)); 
+    this.matIconRegistry.addSvgIcon("SO", this.setPath(`${this.path}/SO.svg`)); 
+    this.matIconRegistry.addSvgIcon("Variable", this.setPath(`${this.path}/Variable.svg`)); 
 
+
+    } 
+
+
+  
+
+   private setPath(url: string): SafeResourceUrl { 
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url); 
+   }
   ngOnInit() {
     // This code loads the IFrame Player API code asynchronously, according to the instructions at
     // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
--- app/src/app/meteo/meteo.component.ts
+++ app/src/app/meteo/meteo.component.ts
@@ -1,9 +1,10 @@
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { MeteoService } from '../services/meteo.service';
 import * as moment from 'moment';
 import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
+import { Forecast, RootForecast } from '../models/MForecast';
 
 
 @Component({
@@ -12,24 +13,70 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./meteo.component.css']
 })
 export class MeteoComponent implements OnInit {
-  name = 'Kissht';
-  KisshtHtml;
+
   current = moment();
-  currentDatas$ : Observable<any>;
-  constructor(private http:HttpClient, private meteoServ: MeteoService, private sanitizer:DomSanitizer) {   
-
-      this.currentDatas$ =  this.meteoServ.getMeteo();
-
-      this.meteoServ.getMeteo2() .subscribe( 
-
-        (v)  => console.log( v )  
+  days = [];
+  modeHours = false;
+  forecast:Array<Forecast> ;
+  currentDatas$ : Observable<RootForecast>;
 
 
-      ) ;
+  constructor( private http:HttpClient, private meteoServ: MeteoService, private sanitizer:DomSanitizer) {   
 
+     
+      this.currentDatas$ = this.meteoServ.getMeteo2();
+
+      let l =  this.current.format('DD') ;
+      let v =  this.current.format('yyyy-MM-DD') ;
+      let obj = { l , v }
+      this.days.push( obj ) ;
+      for(let i = 0 ; i < 5 ; i++ ) {
+       let d =  this.current.add( 1,'days') ;
+       let l =  d.format('DD') ;
+       let v =  d.format('yyyy-MM-DD') ;
+       let obj = { l , v }
+       this.days.push( obj ) ;
+
+      }
+      obj = { l:'D' , v:'-1' };
+      this.days.push( obj ) ;
 
 
     }
+
+
+  todays(d:any) {
+     
+    if( d.v == '-1'  ) {
+      this.modeHours = false ;
+      this.forecast = [] ;
+      return ;
+    }
+
+    this.currentDatas$.pipe(
+     (map  ( res =>  { 
+      let f =  res.properties.forecast;
+      let a =  f.filter( e => {
+        let current = e.time.substring(0, 10) ;
+        return ( current === d.v )
+      }
+      
+      );
+      return a;
+          }  )   )
+
+    ).subscribe( 
+
+      (v) => {
+          this.modeHours = true ;
+          this.forecast = v ;
+      }
+
+    )
+
+  }
+
+
 
   ngOnInit(): void {
   }
