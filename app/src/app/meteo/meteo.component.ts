5b300f93d8e782dcf0982550b44e84a9060bd8b4
 lesech35230 <denis.lesech@gmail.com>
2022-06-10 15:30:52 +0200

    update

--- app/src/app/meteo/meteo.component.ts
+++ app/src/app/meteo/meteo.component.ts
@@ -1,6 +1,6 @@
 import { RootEphe } from './../models/MEphemerides';
 import { Observable, shareReplay } from 'rxjs';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MeteoService } from '../services/meteo.service';
 import * as moment from 'moment';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -8,6 +8,8 @@ import { RootForecast } from '../models/MForecast';
 
 
 import { BullEcheances } from '../models/MBulletin';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { MatTabGroup } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-meteo',
@@ -19,21 +21,51 @@ export class MeteoComponent implements OnInit {
 
   current = moment();
  
-  
+  @ViewChild('tabs') tabGroup: MatTabGroup;
   
   currentDatas$ : Observable<RootForecast>;
   currentBulletin$ : Observable<BullEcheances>;
   currentEphemeride$: Observable<RootEphe>;
 
-  
+  ismobile = false;
 
-  constructor(private meteoServ: MeteoService, private sanitizer:DomSanitizer) {   
+  index = 0 ;
+
+  constructor(private meteoServ: MeteoService, private breakpointObserver: BreakpointObserver ) {   
     
       this.currentDatas$ = this.meteoServ.getMeteo().pipe(shareReplay(1));;
-
+      this.breakpointObserver.observe([
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ]).subscribe(
+        (state: BreakpointState ) => {
+         if ( state.matches ) {
+            this.ismobile = false;
+         } else {
+            this.ismobile = true;
+         }
+        }
+  
+      );
     }
 
 
+   toNext( indice )  {
+    this.index = indice ;
+
+   }
+   
+   next(){
+
+      this.index = this.index + 1;
+      if( this.index > 2 ) {
+        this.index = 0 ;
+      }
+
+      this.tabGroup.selectedIndex = this.index;
+
+    }
 
 
   ngOnInit(): void {
@@ -43,3 +75,5 @@ export class MeteoComponent implements OnInit {
   }
 
 }
+
+
